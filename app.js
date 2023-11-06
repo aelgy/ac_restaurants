c268ab164edc314a9b415292abb8f89ab1124db5
aelgy <aelgy333@gmail.com>
2023-11-06 14:18:41 +0800

    add dynamic route

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.redirect('/restaurants')
+  res.redirect('/restaurants');
 })
 
 app.get('/restaurants', (req, res) => {
-  res.send('find restaurants')
+  res.send('find restaurants');
+})
+
+app.get('/restaurant/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`see restaurant: ${id}`);
 })
 
 app.listen(port, () => {
